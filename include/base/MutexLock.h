1b4fd021d46143174ad474e22ed0cd791b516c27
xu-cell <xu-cell@2459811340@qq.com>
2022-11-08 17:41:29 +0800

    RAII实现锁的封装-自动构造销毁锁，自动上锁和解锁

---
+++ include/base/MutexLock.h
@@ -0,0 +1,62 @@
+#ifndef MUTEXLOCK_H
+#define MUTEXLOCK_H
+#include "noncopyable.h"
+#include <pthread.h>
+#include <cstdio>
+#include "CurrentThread.h"
+class MutexLock : noncopyable
+{
+public:
+    MutexLock():holder(0)
+    {
+        pthread_mutex_init(&mutex_,NULL);
+    }
+    ~MutexLock()
+    {   
+        assert(holder == 0); //确保解完锁之后，锁才能被析构
+        pthread_mutex_destroy(&mutex_);
+    }
+private:
+    void lock() //仅供MutexLockGuard使用
+    {
+        pthread_mutex_lock(&mutex_);
+        holder = CurrentThread::tid(); //顺序不能反
+    }
+    void unlock() //仅供MutexLockGuard使用
+    {
+        holder = 0;
+        pthread_mutex_unlock(&mutex_); // 顺序不能反
+    }
+    pthread_mutex_t* getMutex() //仅供Condition使用
+    {
+        return &mutex_;
+    }
+
+private:
+    pid_t holder;
+    pthread_mutex_t mutex_;
+    friend class Condition;
+    friend class MutexLockGuard;
+};
+
+
+class MutexLockGuard : noncopyable
+{
+public:
+    explicit MutexLockGuard(MutexLock& lock):lock_(lock)
+    {
+        lock_.lock();
+    }
+    ~MutexLockGuard()
+    {
+        lock_.unlock();
+    }
+
+private:
+    MutexLock &lock_; //不允许拷贝构造和赋值，只能传递锁指针
+};
+
+
+
+
+#endif
